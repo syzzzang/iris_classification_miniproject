9a7207750f1258ab8738501d43603c66ea129e85
syzzzang <syzz1105@naver.com>
2023-01-09 16:56:10 +0900

    create main.py : load data

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df.to_csv('iris.csv')
+print(df.head())
