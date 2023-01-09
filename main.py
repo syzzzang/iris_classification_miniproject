338365ff6a205c03601ff67a9d12f864941716eb
syzzzang <syzz1105@naver.com>
2023-01-09 17:06:49 +0900

    update main.py : add target

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ from sklearn.model_selection import train_test_split
 iris = load_iris()
 
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target
 results = df.to_csv('iris.csv')
