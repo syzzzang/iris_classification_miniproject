d0ec4f24b8a3defd57294a84b68caa14eb335278
syzzzang <syzz1105@naver.com>
2023-01-09 16:59:09 +0900

    edit main.py : to csv

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ from sklearn.model_selection import train_test_split
 iris = load_iris()
 
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df.to_csv('iris.csv')
-print(df.head())
+results = df.to_csv('iris.csv')
